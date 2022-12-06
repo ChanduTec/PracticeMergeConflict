bd439d63f953b1bbded5142642799e4051ee695f
unknown <chandu.chowdary622@gmail.com>
2022-12-06 17:21:17 +0530

    First Commit

---
+++ src/com/info/Beautiful.java
@@ -0,0 +1,12 @@
+package com.info;
+
+public class Beautiful {
+
+	static {
+		System.out.println("Beautiful.enclosing_method()");
+	}
+	
+	public static void hi() {
+		System.out.println("Beautiful.hi()");
+	}
+}
---
+++ src/com/info/Confuse.java
@@ -0,0 +1,12 @@
+package com.info;
+
+public class Confuse {
+
+	static {
+		System.out.println("Confuse.enclosing_method()");
+	}
+	
+	public static void avoid() {
+		System.out.println("Confuse.avoid()");
+	}
+}
